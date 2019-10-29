6e4ab8d4a536b4c05dafaf253c62c31699ab6f17
bagwanbulbul <50580040+bagwanbulbul@users.noreply.github.com>
2019-10-29 09:23:21 +0530

    Delete if_else_Q4.py

--- if_else_Q4.py
+++
@@ -1,8 +0,0 @@
-water = int(raw_input("enter water level"))
-if (water<1l):
-	print ("paani bhrna hai")
-elif (water>1l and water< 10l):
-	print ("paani nhi bhrna hai")
-else:
-	print ("overflow")
-
